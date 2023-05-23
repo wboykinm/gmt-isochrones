d5b6870d5b3f26c70636a09340982ce2a0b24edd
Bill Morris <bill@billmorris.io>
2023-05-23 09:47:38 -0400

    philly

--- scripts/calculate-station-times.R
+++ scripts/calculate-station-times.R
@@ -1,31 +1,39 @@
 library (gtfsrouter)
 packageVersion ("gtfsrouter")
 library(lubridate)
+library('plyr')
 library('dplyr')
 library('stringr')
-library('plyr')
 
 # Pull from the latest MTA subway GTFS release
-url <- "http://web.mta.info/developers/data/nyct/subway/google_transit.zip"
+url <- "https://github.com/septadev/GTFS/releases/latest/download/gtfs_public.zip#google_rail.zip"
+# url <- "http://web.mta.info/developers/data/nyct/subway/google_transit.zip"
 f <- "google_transit.zip"
 if (!file.exists (f)) download.file (url, destfile = f)
 
+parentFolder<-"tmp_parent"
+unzip(f,exdir=parentFolder)
+trainzip<-paste0(parentFolder,"/google_rail.zip")
+
+# Separately extract the feed to access and iterate over the "stops.txt" table
+outDir<-"tmp_data"
+unzip(trainzip,exdir=outDir)
+stops <- read.csv("tmp_data/stops.txt")
+
 # Extract the feed
-gtfs <- extract_gtfs (f, stn_suffixes = c ("N", "S"))
+gtfs <- extract_gtfs (trainzip)
 
 # Buld a timetable
 gtfs <- gtfs_timetable (gtfs, day = "Wed")
 
-# Separately extract the feed to access and iterate over the "stops.txt" table
-outDir<-"tmp_data"
-unzip(f,exdir=outDir)
-stops <- read.csv("tmp_data/stops.txt")
+# Build a transfer table
+gtfs <- gtfs_transfer_table( gtfs, d_limit = 500 )
 
 # Filter the stops
-stops <- filter(stops, !str_detect(stop_id, "(N|S)$"))
-stops <- filter(stops, !str_detect(stop_id, "140$"))
-stops <- filter(stops, !str_detect(stop_id, "H19$"))
-stops <- filter(stops, !str_detect(stop_id, "N12$"))
+# stops <- filter(stops, !str_detect(stop_id, "(N|S)$"))
+# stops <- filter(stops, !str_detect(stop_id, "140$"))
+# stops <- filter(stops, !str_detect(stop_id, "H19$"))
+# stops <- filter(stops, !str_detect(stop_id, "N12$"))
 
 # print(stops)
 
